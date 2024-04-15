c02f4a3de5f1e8ea154213f88d5fcdc77a467518
Echo <echo-the-coder@tuta.io>
2024-04-14 20:08:19 -0400

    404 handling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 console.log('%c BillsBoards', 'font: 2em sans-serif; color: #ff0000;')
+
+if (!window.location.pathname === "/") {
+  window.location.href = "/";
+}
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
