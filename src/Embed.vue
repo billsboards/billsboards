98bc59736341df5320317becb917f7f9273023fc
Echo <echo-the-coder@tuta.io>
2024-04-16 10:32:43 -0400

    Changes and stuff

--- src/Embed.vue
+++ src/Embed.vue
@@ -3,6 +3,7 @@
     <img class="w-full h-full object-cover" :src="imageUrl" alt="Random Image" />
   </div>
 </template>
+
 <script>
 import { ref, onMounted } from 'vue'
 
@@ -13,11 +14,13 @@ export default {
     const imageUrl = ref('')
 
     onMounted(() => {
-      const urlParams = new URLSearchParams(window.location.search)
-      refParam.value = urlParams.get('ref')
       const randomNumber = Math.floor(Math.random() * 3) + 1
       imageUrl.value = `https://cdn.billsboards.net/${randomNumber}.png`
+      const urlParams = new URLSearchParams(window.location.search)
+      refParam.value = urlParams.get('ref')
+      console.log("Domain log" + refParam.value)
     })
+
     return {
       ref: refParam,
       imageUrl
--- src/embed.js
+++ src/embed.js
@@ -1,6 +1,5 @@
-console.log('%c BillsBoards', 'font: 2em sans-serif; color: #ff0000;')
-console.log('Get more info at billsboards.net')
-import { createApp } from 'vue'
-import Embed from './Embed.vue'
+console.log("%c BillsBoards","font: 2em sans-serif; color: #ff0000;", "\nGet more info at billsboards.net");
+import { createApp } from "vue";
+import Embed from "./Embed.vue";
 
-createApp(Embed).mount('#embed')
+createApp(Embed).mount("#embed");
