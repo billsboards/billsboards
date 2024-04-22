866cfd20a342f1f97ea605b27b1d976d6ef5e914
Echo <echo-the-coder@tuta.io>
2024-04-22 14:44:00 -0400

    Fix Tailwind CSS

--- src/embed.js
+++ src/embed.js
@@ -1,5 +1,6 @@
 console.log("Very real adverts done by BillsBoards. Get more info at billsboards.net");
 import { createApp } from "vue";
+import './style.css'
 import Embed from "./Embed.vue";
 
 createApp(Embed).mount("#embed");
