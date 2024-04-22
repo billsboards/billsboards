c078c02892a1a7833bb92b60892621b227f9d17e
Echo <echo-the-coder@tuta.io>
2024-04-22 15:50:08 -0400

    Minor embed changes and more

--- src/Embed.vue
+++ src/Embed.vue
@@ -1,58 +1,62 @@
 <template>
   <div class="relative">
-    <img class="w-full h-full object-cover" :src="state.imageUrl" alt="Random Image" />
-    <div class="absolute top-0 right-0 p-2">
-      <a href="https://billsboards.net" target="_blank" class="text-white text-xs font-bold bg-gray-700 py-1 px-2 rounded-lg">BB</a>
+    <a :href="state.adUrl" target="_blank" class="w-[728px] h-[90px]">
+      <img class="w-[728px] h-[90px] object-cover" :src="state.imageUrl" title="BillsBoard" alt="BillsBoard" />
+    </a>
+    <div class="absolute top-0 right-0 p-0.5">
+      <a href="https://billsboards.net" target="_blank" title="Learn about BillsBoards" aria-label="BillsBoard link" class="text-white text-xs font-bold bg-gray-700 py-1 px-2 rounded-lg">BB</a>
     </div>
   </div>
 </template>
-
 <script>
-import { onMounted, reactive } from "vue";
-import { getDatabase, ref as dbRef, runTransaction } from "firebase/database";
-import { initializeApp } from "firebase/app";
+  import { onMounted, reactive } from "vue";
+  import { getDatabase, ref as dbRef, runTransaction } from "firebase/database";
+  import { initializeApp } from "firebase/app";
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkW3nOPRxHEQ-fmYXCCo4Xas5iU4b7cxk",
+    authDomain: "billsboards-63569.firebaseapp.com",
+    databaseURL: "https://billsboards-63569-default-rtdb.firebaseio.com",
+    projectId: "billsboards-63569",
+    appId: "1:619901129471:web:a626559674a81b0d4f32ca",
+  };
+  // could we offload this to server?
+  // probs, but i dont wanna
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCkW3nOPRxHEQ-fmYXCCo4Xas5iU4b7cxk",
-  authDomain: "billsboards-63569.firebaseapp.com",
-  databaseURL: "https://billsboards-63569-default-rtdb.firebaseio.com",
-  projectId: "billsboards-63569",
-  appId: "1:619901129471:web:a626559674a81b0d4f32ca"
-};
+  function addView(domain) {
+    const cleanRef = domain.replace(/\./g, "_");
+    const ref = dbRef(db, `db1/${cleanRef}`);
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-function addView(domain) {
-  const cleanRef = domain.replace(/\./g, '_');
-  const ref = dbRef(db, `db1/${cleanRef}`);
-
-  runTransaction(ref, (views) => {
-    return (views || 0) + 1;
-  });
-}
-
-export default {
-  name: "Embed",
-  setup() {
-    const state = reactive({
-      ref: null,
-      imageUrl: "",
+    runTransaction(ref, (views) => {
+      return (views || 0) + 1;
     });
+  }
 
-    onMounted(() => {
-      const ranNum = Math.floor(Math.random() * 3) + 1;
-      state.imageUrl = `https://cdn.billsboards.net/${ranNum}.png`;
-      const urlPar = new URLSearchParams(window.location.search);
-      state.ref = urlPar.get("ref");
-      addView(state.ref);
-    });
+  const cdn = "https://cdn.billsboards.net/";
+  export default {
+    name: "Embed",
+    setup() {
+      const state = reactive({
+        ref: null,
+        adUrl: "",
+        imageUrl: "",
+      });
 
-    return {
-      state
-    };
-  },
-};
+      onMounted(async () => {
+        const ranNum = Math.floor(Math.random() * 3) + 1;
+        state.imageUrl = `${cdn}${ranNum}.png`;
+        const urlPar = new URLSearchParams(window.location.search);
+        state.ref = urlPar.get("ref");
+        addView(state.ref);
+        const response = await fetch(`${cdn}/db/${ranNum}.txt`);
+        state.adUrl = await response.text();
+      });
 
-// TODO: Add leaderboard and optimize data queries
+      return {
+        state,
+      };
+    },
+  };
+  // TODO: Add leaderboard and optimize data queries
 </script>
