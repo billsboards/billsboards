d7ce87f815025e3215976da77294c5636b3915d1
Echo <echo-the-coder@tuta.io>
2024-04-21 23:45:25 -0400

    Add firebase API

--- src/Embed.vue
+++ src/Embed.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="absolute inset-0 p-0 m-0">
-    <img class="w-full h-full object-cover" :src="imageUrl" alt="Random Image" />
+    <img class="w-full h-full object-cover" :src="state.imageUrl" alt="Random Image" />
   </div>
 </template>
-
 <script>
-import { ref, onMounted } from 'vue'
+  import { onMounted, reactive } from "vue";
+  import { getDatabase, ref as dbRef, runTransaction } from "firebase/database";
+  import { initializeApp } from "firebase/app";
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkW3nOPRxHEQ-fmYXCCo4Xas5iU4b7cxk",
+    authDomain: "billsboards-63569.firebaseapp.com",
+    databaseURL: "https://billsboards-63569-default-rtdb.firebaseio.com",
+    projectId: "billsboards-63569",
+    appId: "1:619901129471:web:a626559674a81b0d4f32ca"
+  };
 
-export default {
-  name: 'Embed',
-  setup() {
-    const refParam = ref(null)
-    const imageUrl = ref('')
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
 
-    onMounted(() => {
-      const randomNumber = Math.floor(Math.random() * 3) + 1
-      imageUrl.value = `https://cdn.billsboards.net/${randomNumber}.png`
-      const urlParams = new URLSearchParams(window.location.search)
-      refParam.value = urlParams.get('ref')
-      console.log("Domain log" + refParam.value)
-    })
-
-    return {
-      ref: refParam,
-      imageUrl
-    }
+  function addView(domain) {
+    const cleanRef = domain.replace(/\./g, '_');
+    const ref = dbRef(db, `db1/${cleanRef}`);
+    runTransaction(ref, (views) => {
+      return (views || 0) + 1;
+    });
   }
-}
+
+  export default {
+    name: "Embed",
+    setup() {
+      const state = reactive({
+        ref: null,
+        imageUrl: ""
+      });
+      onMounted(() => {
+        const ranNum = Math.floor(Math.random() * 3) + 1;
+        state.imageUrl = `https://cdn.billsboards.net/${ranNum}.png`;
+        const urlPar = new URLSearchParams(window.location.search);
+        state.ref = urlPar.get("ref");
+        addView(state.ref);
+      });
+      return {
+        state
+      };
+    },
+  };
+  // TODO: Add leaderboard and optimize data queries
 </script>
