bc096f3a9bf46137cfb8286e9fdfe1240cef582c
proudparrot2 <hi@proudparrot2.tech>
2024-05-15 00:05:43 -0500

    FAQ

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,13 @@ const people = [
     views: 36
   }
 ]
+
+const accordion = [
+  { label: 'How do I put my image on a Board?', content: 'You can open a pull request on our CDN repository, https://github.com/billsboard/cdn' },
+  { label: 'Why should I enter my domain name?', content: 'By entering your domain name, we can track the amount of views your Board receives and place you on the leaderboard accordingly. If you don\'t want to track pageviews, you can leave the domain name blank and copy the embed code as normal.' },
+  { label: 'What\'s the point of this?', content: 'To bring a little more fun to your personal portfolio or website, and to host a little friendly competition on the leaderboard.' },
+  { label: 'Do you track data?', content: 'The Board\'s embed code is an iframe to this website, which just returns an image. We only collect the amount of views that the embed receives under your domain name to host on the leaderboard. If you want your domain\'s data removed, please send an email to "contact@billsboards.net". You can also see the full source code for this website at link at the bottom of the page.' }
+]
 </script>
 <template>
   <div class="space-y-8 text-white prose-headings:text-white">
@@ -30,7 +37,9 @@ const people = [
       (noun): A image that should be viewed or placed on every website.
     </blockquote>
 
-    <p>It is recommended that all people who are not silly should avoid this website. People who are silly should view billsboards and place their own billsboards on sites they control. Failure to do so may result in a lack of silliness.</p>
+    <p>It is recommended that all people who are not silly should avoid this website. People who are silly should view
+      billsboards and place their own billsboards on sites they control. Failure to do so may result in a lack of
+      silliness.</p>
 
     <div class="w-full">
       <UTable :rows="people" />
@@ -47,10 +56,14 @@ const people = [
       <span v-else>Copied!</span>
     </UButton>
 
+    <UAccordion :items="accordion" size="lg" variant="solid" color="white" />
+
     <UDivider />
 
     <div class="text-sm text-white/50">
       Made by Echo and proudparrot2 <br />
-      Open-source on <ULink variant="white" to="https://github.com/billsboards/billsboards" class="underline underline-offset-4">GitHub</ULink></div>
+      Open-source on <ULink variant="white" to="https://github.com/billsboards/billsboards"
+        class="underline underline-offset-4">GitHub</ULink>
+    </div>
   </div>
 </template>
